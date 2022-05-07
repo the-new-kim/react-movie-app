ee701d8c1428603f2fd04ae62c3cf9950957d41e
the-new-kim <jonkim0309@gmail.com>
2022-05-07 21:57:04 +0900

    react router dom installed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState();
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie key={movie.id} title={movie.title} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,31 @@
-function Movie() {
-  return null;
+import Proptypes from "prop-types";
+
+function Movie({ title, coverImg, year, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title}></img>
+      <h2>
+        {title}({year})
+      </h2>
+      <p>{summary}</p>
+      {!genres.length ? null : (
+        <ul>
+          {genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+      )}
+      <hr></hr>
+    </div>
+  );
 }
+
+Movie.propTypes = {
+  title: Proptypes.string.isRequired,
+  coverImg: Proptypes.string.isRequired,
+  year: Proptypes.number.isRequired,
+  summary: Proptypes.string.isRequired,
+  genre: Proptypes.arrayOf(Proptypes.string),
+};
 
 export default Movie;
